171a3c972fd22b13a718467f943fe21c5b7f52d8
anomaliesintent112 <109564945+anomaliesintent112@users.noreply.github.com>
2024-05-17 19:12:08 +0000

    Update BIGIP_decode_cookie.py to python3

--- BIGIP_decode_cookie.py
+++ BIGIP_decode_cookie.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python
 
-# example string: BIGip<ervername>110536896.20480.0000
+# example string: BIGip<ervername>=110536896.20480.0000
 
 import struct
 import sys
 import re
 
 if len(sys.argv) != 2:
-    print "Usage: %s cookie" % sys.argv[0]
+    print(f"Usage: {sys.argv[0]} cookie")
     exit(1)
 
 cookie = sys.argv[1]
-print "\n[*] Cookie to decode: %s\n" % cookie
+print(f"\n[*] Cookie to decode: {cookie}\n")
 
 (cookie_name, cookie_value) = cookie.split('=')
 
 pool = re.search('^BIGipServer([.\w\.]*)', cookie_name)
 
+if pool is None:
+    print("Error: Cookie name does not match expected format.")
+    exit(1)
+
 (host, port, end) = cookie_value.split('.')
 
-(a, b, c, d) = [ord(i) for i in struct.pack("<I", int(host))]
+(a, b, c, d) = [i for i in struct.pack("<I", int(host))]
 
-(e) = [ord(e) for e in struct.pack("<H", int(port))]
-port = "0x%02X%02X" % (e[0],e[1])
+(e1, e2) = struct.pack("<H", int(port))
+port = "0x%02X%02X" % (e1, e2)
 
-print "[*] Pool name: %s" % (pool.group(1))
-print "[*] Decoded IP and Port: %s.%s.%s.%s:%s\n" % (a,b,c,d, int(port,16))
+print(f"[*] Pool name: {pool.group(1)}")
+print(f"[*] Decoded IP and Port: {a}.{b}.{c}.{d}:{int(port, 16)}\n")
